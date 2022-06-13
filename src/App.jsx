db9d6da071c5f7bc417e84ff268626a82eda099a
Felipe de Sousa Fernandes <fesousalocutor@gmail.com>
2022-06-13 02:57:37 -0300

    upload storage firebase

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
+import { storage } from './firebase.js'
+import './App.css'
+
+function App() {
+  const [imgURL, setImgURL] = useState("")
+  const [progress, setProgress] = useState(0)
+
+  const handleUpload = event => {
+    event.preventDefault()
+    const file = event.target[0]?.files[0]
+    if(!file) return
+
+    const storageRef = ref(storage, `images/${file.name}`)
+    const uploadTask = uploadBytesResumable(storageRef, file)
+
+    uploadTask.on("state_changed", snapshot => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes * 100)
+      setProgress(progress)
+    }, error => {
+      alert(error)
+    },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(url => {
+          setImgURL(url)
+        })
+      }
+    )
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form onSubmit={handleUpload}>
+          <input type="file" /><br/>
+          <button type="submit">Upload</button>
+        </form>
+        <br />
+        {!imgURL  && <progress value={progress} max="100"/>}
+        {imgURL && <img src={imgURL} alt="Imagem upload" height={300}/>}
+      </header>
+    </div>
+  )
+}
+
+export default App
